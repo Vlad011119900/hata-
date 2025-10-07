0a22ab083c36dc85305f38fb3948b70538518efc
vlad01111990 <vladyavorskyi84@gmail.com>
2025-10-07 17:58:23 +0300

    commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+popupopen.onclick = () => {
+    darkbgc.style.display = "block"
+    popup.style.display = "block"
+}
+
+cancel.onclick = () => {
+    darkbgc.style.display = "none"
+    popup.style.display = "none"
+}
+
+darkbgc.onclick = () => {
+    darkbgc.style.display = "none"
+    popup.style.display = "none"
+}
