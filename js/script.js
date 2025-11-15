1675b04f5e0783811f8fe4cb21bbcfe87e60afb4
vlad01111990 <vladyavorskyi84@gmail.com>
2025-11-15 22:22:09 +0200

    finished

--- js/script.js
+++ js/script.js
@@ -12,3 +12,18 @@ darkbgc.onclick = () => {
     darkbgc.style.display = "none"
     popup.style.display = "none"
 }
+
+burger.onclick = () => {
+    darkbgc.style.display = "block"
+    menu1.style.left = "0"
+}
+
+burgercancel.onclick = () => {
+    darkbgc.style.display = "none"
+    menu1.style.left = "-1000px"
+}
+
+darkbgc.onclick = () => {
+    darkbgc.style.display = "none"
+    menu1.style.left = "-1000px"
+}
